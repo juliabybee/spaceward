5a779a602d3eb63bcb1f6f508bf6077f55714efa
juliabybee <106109681+juliabybee@users.noreply.github.com>
2024-10-05 14:11:39 -0500

    Add files via upload

---
+++ final_project/src/main/java/com/example/spaceward/SpacewardApplication.java
@@ -0,0 +1,13 @@
+package com.example.spaceward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacewardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacewardApplication.class, args);
+	}
+
+}
---
+++ final_project/src/test/java/com/example/spaceward/SpacewardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spaceward;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacewardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
